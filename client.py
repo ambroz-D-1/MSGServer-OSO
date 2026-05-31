8344c6080018bf4b16ac702f323df1d6171ff5ea
MBAir <283878@student.pwr.edu.pl>
2026-05-31 16:33:23 +0200

    Updated client for testing

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import json
 import os
 import socket
+from time import sleep
 from server_messages import TEXT, ACTION, make_message
 load_dotenv()
 
@@ -22,15 +23,22 @@ passwd="elo"
 
 listUsers=make_message(content="",sender=user,recipient="Server",action=ACTION["listAllUsers"])
 login="""{"action":"login", "properties":{"login":"Admin01", "password":"elo"}}""".encode()
-
+listOnline=make_message(content="",sender="Client",recipient="Server", action=ACTION["listOnlineUsers"])
 # json_packet="""{"action":"register","properties":{"login":"Rassena","password":"elozelo"}}"""
 server.send(login)
+print(server.recv(1024).decode())
+sleep(1)
 server.send(listUsers)
+print(server.recv(1024).decode())
+sleep(1)
+server.send(listOnline)
+print(server.recv(1024).decode())
+sleep(1)
 
 # while input()!='exit':
 while True:
     msg=server.recv(1024).decode()
-    print(msg)
+    print(msg+"\n")
     server.send(input().encode())
 
 server.close()
